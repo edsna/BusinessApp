88cf46b4ab6210f77de8dfda5fb16f636d04dc61
edsna <edsonaguiar17@gmail.com>
2019-07-17 23:50:46 +0530

    Link is working just fine

--- app/src/main/java/com/edsonz/domains/trincoll/edu/businessapp/MainActivity.java
+++ app/src/main/java/com/edsonz/domains/trincoll/edu/businessapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.edsonz.domains.trincoll.edu.businessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;  //Allows click events in the app
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hyperLink; //defines hyperLink variable
+    Spanned text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hyperLink = (TextView) findViewById(R.id.myWebsite); //Assigns var hyperLink to TextView in XML
+        text = Html.fromHtml("<a href='https://www.android-examples.com//'>Visit our Website</a>");
+
+        hyperLink.setMovementMethod(LinkMovementMethod.getInstance());
+        hyperLink.setText(text);
     }
 }
